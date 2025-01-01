5f74451f34797e895b38de26b53359267f24fe0e
Khaled <khaled.m2225@gmail.com>
2025-01-01 23:12:30 +0200

    Initial Commit

---
+++ iOS-Jan-2025/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOS-Jan-2025
+//
+//  Created by Khaled Mohamed on 01/01/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOS-Jan-2025/iOS_Jan_2025App.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Jan_2025App.swift
+//  iOS-Jan-2025
+//
+//  Created by Khaled Mohamed on 01/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct iOS_Jan_2025App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
